17fea6deab15849c34f39760ec5cff284a308f40
Marius Bongarts <mariusbongarts@web.de>
2022-05-08 17:51:27 +0200

    change seconds

--- src/templates/dart-board.ts
+++ src/templates/dart-board.ts
@@ -2,7 +2,7 @@ import { SetOfDarts } from '../dart-calculator';
 
 const BOARD_WIDTH = 787;
 const BOARD_HEIGHT = 774;
-const MAX_X_Y = 2.4;
+const MAX_X_Y = 2.6;
 export const dartBoardTemplate = (darts: SetOfDarts) => `
 <!DOCTYPE html>
 <html lang="en">
